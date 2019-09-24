3535763ed92589f94208f33fd7c48f9fc6822182
mmb-96 <mmb_96@hotmail.com>
2019-09-24 11:57:58 +0200

    Ejercicio Eventos finalizados

--- JavaScript/Eventos/Ejercicio 1/scripts.js
+++ JavaScript/Eventos/Ejercicio 1/scripts.js
@@ -1,3 +1,4 @@
+//Ejercicio 1
 const caja = document.getElementById("box");
 
 caja.addEventListener("mouseenter" ,() => {
@@ -16,17 +17,37 @@ caja.addEventListener("mouseup" ,() => {
     console.log("Has soltado el botón izquierdo dentro de la caja.");
 });
 
+//Ejercicio 2
 const text = document.getElementById("texto");
 
 text.addEventListener("keydown" ,() =>{
     console.log("Has pulsado una tecla.");
-    console.log("Se ha pulasado la tecle: " + String.fromCharCode(event.keyCode));
+    console.log("Se ha pulasado la tecla: " + event.key);
 });
 
-text.addEventListener("keydown" ,() =>{
+text.addEventListener("keyup" ,() =>{
     console.log("Has soltado una tecla.");
 });
 
+//Ejercicio 3
+let cadena;
+
 const escrito = () => {
     const text = document.getElementById("text2");
-}
+    text.addEventListener("keyup" ,() =>{
+        let letra = event.key;
+        if (cadena == undefined) {
+            cadena = letra;
+        } else {
+            cadena += letra;
+        }
+        console.log(letra)
+        console.log(cadena);
+    })
+    
+};
+
+const  button = document.getElementById("boton");
+button.addEventListener('click', () => {
+    escrito();
+});
