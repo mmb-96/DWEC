494215de9a27f68710455a67e4a923e6873716ea
mmb-96 <mmb_96@hotmail.com>
2019-09-24 16:26:14 +0200

    Terminado galeria

--- JavaScript/Eventos/Galeria/scripts.js
+++ JavaScript/Eventos/Galeria/scripts.js
@@ -1,1 +1,6 @@
 /* cambiar el color a rojo y en vez de un puntero a una mano*/
+const gale = document.getElementById("gallery");
+
+gale.addEventListener("click" , (color) =>{
+    color.target.classList.toggle("red");
+})
