a74e0edb09e47f8700720c59632073f50efc9ff3
mmb-96 <mmb_96@hotmail.com>
2019-10-05 13:15:13 +0200

    Ejemplo callback

---
+++ JavaScript/ejcb.js
@@ -0,0 +1,19 @@
+const getUser = (id, cb) => {
+    const user = {
+        name: 'Dorian',
+        id: id
+    }
+
+    if (id == 2) cb('User not exist')
+    else cb(null, user)
+}
+
+getUser(1,(err,user) => {
+    if(err) return console.log(err)
+    else console.log ('User name is ' + user.name)
+})
+
+getUser(2,(err,user) => {
+    if(err) return console.log(err)
+    else console.log ('User name is ' + user.name)
+})
